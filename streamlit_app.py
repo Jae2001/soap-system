d47303bdcf0c3db13ca1d8506b7de8cb896dd45a
Jae2001 <jaecareteam@gmail.com>
2026-01-14 12:10:38 +0800

    Add initial Streamlit app

---
+++ streamlit_app.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+st.set_page_config(page_title="Soap System", layout="wide")
+
+st.title("🧼 Soap Management System")
+st.success("Streamlit Cloud connected successfully!")
+
+st.write("""
+This system will manage:
+- Ingredients stock
+- Purchases
+- Recipes
+- Production
+- Sales & reports
+""")
